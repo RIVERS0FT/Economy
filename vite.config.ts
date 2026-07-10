4e897696ab56c65130e62c7f3f5037786ebb185e
RIVERSOFT <105160096+RIVERS0FT@users.noreply.github.com>
2026-07-10 18:48:56 +0800

    Deploy Economy from subdomain root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/economy/',
+  base: '/',
   plugins: [react()],
   clearScreen: false,
   server: {
