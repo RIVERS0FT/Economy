46ecdfd79f9a3078c64d5bd81e17d104fc3927b6
RIVERSOFT <105160096+RIVERS0FT@users.noreply.github.com>
2026-07-10 18:47:30 +0800

    Configure Economy for /economy/ web deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/economy/',
   plugins: [react()],
   clearScreen: false,
   server: {
