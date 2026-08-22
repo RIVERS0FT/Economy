db214151f33e878baa5991292fb9302be020a41d
RIVERSOFT <105160096+RIVERS0FT@users.noreply.github.com>
2026-08-22 15:18:04 +0800

    fix: collapse desktop sidebar on page navigation

--- src/components/shell/GameShell.tsx
+++ src/components/shell/GameShell.tsx
@@ -155,6 +155,10 @@ export function GameShell({ model, children, offline = false }: {
     return () => controller.abort();
   }, [offline]);
 
+  useLayoutEffect(() => {
+    setSidebarCollapsed(true);
+  }, [model.tab]);
+
   useEffect(() => {
     notificationCenter.closePanel();
   }, [model.tab, notificationCenter.closePanel]);
